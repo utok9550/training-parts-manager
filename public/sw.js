2024485244dc21c423dc49d8ddff9f75f55fa486
utok9550 <utok9550@gmail.com>
2026-06-17 22:31:56 +0900

    PWAキャッシュ名を更新

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "training-parts-manager-v1";
+const CACHE_NAME = "training-parts-manager-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
